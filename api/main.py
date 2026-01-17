4346ec7bf9066b4487080254308d9211606c6762
Ygor <ygorsilvad.souza@gmail.com>
2026-01-17 19:39:21 -0300

    Ajustando códigos

--- api/main.py
+++ api/main.py
@@ -2,86 +2,81 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 
-app = FastAPI(title="API Bancária")
+app = FastAPI(title= "API Bancária")
 
-# Modelos de dados
 class Account(BaseModel):
-    id: int
-    number: str
-    agency: str
-    balance: float
-    limit: float
+  id: int
+  number: str
+  agency: str
+  balance: float
+  limit: float
 
 class Card(BaseModel):
-    id: int
-    number: str
-    limit: float
+  id: int
+  number: str
+  limit: float
 
 class Client(BaseModel):
-    id: int
-    nome: str
-    account: Account
-    card: Card
-    features: List[str] = []
-    news: List[str] = []
+  id: int
+  name: str
+  account: Account
+  card: Card
+  features: list[str] = []
+  news: List[str] = []
 
-
-#Banco em memória
 clientes_db: List[Client] = []
 
 
+# http://127.0.0.1:8000/rota
 
-
-#POST (CREATE)
 
 @app.post("/cliente")
 def criar_cliente(cliente: Client):
-    # evita ID duplicado
-    for c in clientes_db:
-        if c.id == cliente.id:
-            raise HTTPException(status_code=400, detail="Cliente já existe")
 
-    clientes_db.append(cliente)
-    return {"mensagem": "Cliente criado com sucesso", "cliente": cliente}
-
-
-#GET (READ)
+  for c in clientes_db:
+    if c.id == cliente.id:
+      raise HTTPException(status_code=400, detail="Cliente já existe")
+  
+  clientes_db.append(cliente)
+  return {"mensagem": "Cliente criado com sucesso", "cliente": cliente}
 
 @app.get("/clientes")
 def listar_clientes():
-    return clientes_db
+  return clientes_db
 
 
 @app.get("/cliente/{cliente_id}")
 def obter_cliente(cliente_id: int):
-    for cliente in clientes_db:
-        if cliente.id == cliente_id:
-            return cliente
-
-    raise HTTPException(status_code=404, detail="Cliente não encontrado")
-
-
-#DELETE
+  for cliente in clientes_db:
+    if cliente.id == cliente_id:
+      return cliente
+    
+  raise HTTPException(status_code=404, detail="Cliente não encontrado")
 
 @app.delete("/cliente/{cliente_id}")
 def deletar_cliente(cliente_id: int):
-    for index, cliente in enumerate(clientes_db):
-        if cliente.id == cliente_id:
-            clientes_db.pop(index)
-            return {"mensagem": "Cliente removido com sucesso"}
-
-    raise HTTPException(status_code=404, detail="Cliente não encontrado")
+  for index, cliente in enumerate(clientes_db):
+    if cliente.id == cliente_id:
+      clientes_db.pop(index)
+      return {"mensagem": "Cliente removido com sucesso"}
+  
+  raise HTTPException(status_code=4004, detail="Cliente não encontrado")
 
 @app.put("/cliente/{cliente_id}")
 def atualizar_cliente(cliente_id: int, cliente_atualizado: Client):
 
-    for index, cliente in enumerate(clientes_db):
-        if cliente.id == cliente_id:
-            cliente_atualizado.id = cliente_id
-            clientes_db[index] = cliente_atualizado
-            return {
-                "mensagem": "Cliente atualizado com sucesso",
-                "cliente": cliente_atualizado
-            }
+  for index, cliente in enumerate(clientes_db):
+    if cliente.id == cliente_id:
 
-    raise HTTPException(status_code=404, detail="Cliente não encontrado")
+      cliente_atualizado.id = cliente_id
+
+      clientes_db[index] = cliente_atualizado
+
+      return{
+        "mensagem": "Cliente atualizado com sucesso",
+        "cliente": cliente_atualizado
+      }
+    
+  raise HTTPException(status_code=404, detail="Cliente não encontrado")
+
+# uvicorn main:app --reload - rodar a api
--- etl/load.py
+++ etl/load.py
@@ -1,58 +1,63 @@
-import requests
-from transform import users
+#Aqui são feitos os imports da API e biblioteca do Gemini
+# e do extract.py criado no projeto
+import google.genai as genai
+from google.genai import types
+import extract
 
-API_URL = "http://127.0.0.1:8000"
+#Chave da API
+API_KEY = ""
 
+# Inicializa o client
+client = genai.Client(api_key=API_KEY)
 
-def carregar_mensagem_no_cliente(user: dict):
-    cliente_id = user.get("id")
-    mensagem = user.get("mensagem")
+def gerar_mensagem(user: dict) -> str: 
+    #recebe um usuario(dicionário) e retorna uma string
+    
+    
+    #Pega os dados do usuário de forma segura
+    nome = user.get("name", "cliente") 
+    saldo = user.get("account", {}).get("balance", 0.0)
 
-    if not cliente_id:
-        print("Usuário sem ID. Ignorado.")
-        return
-
-    if not mensagem:
-        print(f"Cliente {cliente_id} sem mensagem. Ignorado.")
-        return
-
-    # Busca cliente atual na API
-    response = requests.get(f"{API_URL}/cliente/{cliente_id}")
-
-    if response.status_code != 200:
-        print(f"Cliente {cliente_id} não encontrado na API.")
-        return
-
-    cliente = response.json()
-
-    # Evita duplicar mensagens
-    if mensagem in cliente.get("news", []):
-        print(f"Mensagem já existe para o cliente {cliente['name']}.")
-        return
-
-    # Adiciona a mensagem ao campo news
-    cliente["news"].append(mensagem)
-
-    # Atualiza o cliente via PUT
-    put_response = requests.put(
-        f"{API_URL}/cliente/{cliente_id}",
-        json=cliente
+    #Da as instruções para a ia de como proceder na resposta
+    prompt = (
+        f"Escreva UMA mensagem curta (máx. 25 palavras), "
+        f"profissional e amigável, para o cliente {nome}, "
+        f"informando que seu saldo atual é R${saldo:.2f}."
     )
 
-    if put_response.status_code == 200:
-        print(
-        f"Mensagem carregada com sucesso para o cliente "
-        f"{cliente.get('name', 'cliente')}"
-)
-
-    else:
-        print(
-            f"Erro ao atualizar cliente {cliente_id}: "
-            f"{put_response.status_code} - {put_response.text}"
+    #Chamada à API GEMINI
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-lite",
+            contents=prompt
         )
 
+        #Tratamento da respota
+        texto = response.text.strip() #remove espaços extras
+        return texto[:200]  # proteção extra contra resposta longa
+    except Exception as e:
+        return f"Erro ao gerar mensagem: {e}"
+    #se acabar a quota, cair a internet ou a API falhar o programa não quebra
 
-# ===== EXECUÇÃO DO LOAD =====
-if __name__ == "__main__":
-    for user in users:
-        carregar_mensagem_no_cliente(user)
+
+#ETAPA DE EXTRAÇÃO
+users = extract.fetch_users() #Chama o fetch do extract.py com a busca dos usuários em json para lista
+
+#Se não achar usuários faz o exit() 
+if not users:
+    print("Atenção: Nenhum usuário foi encontrado na extração.")
+    exit()
+
+#ETAPA DE TRANSFORMAÇÃO
+#Vai percorrer cada cliente e gerar uma mensagem com a IA enrriquecendo o dado
+for user in users:
+    user["mensagem"] = gerar_mensagem(user)
+
+#ETAPA DE CARGA / VISUALIZAÇÃO
+#Confirma que funciounou
+print(f"Sucesso! Mensagem do primeiro usuário:\n{users[0]['mensagem']}\n")
+
+#Carrega no terminal
+for user in users:
+    print(f"Cliente: {user['name']} | Mensagem: {user['mensagem']}")
+
